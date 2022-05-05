b572368fc03fe165f4b36db2f803f764e5f3c134
Oleg Tsybulskyi <helg37@gmail.com>
2022-05-05 17:10:12 +0300

    Init database.

--- ShopOnline.Api/Program.cs
+++ ShopOnline.Api/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using ShopOnline.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContextPool<ShopOnlineDbContext>(options => 
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("ShopOnlineConnection")
+    )
+);
 
 var app = builder.Build();
 
