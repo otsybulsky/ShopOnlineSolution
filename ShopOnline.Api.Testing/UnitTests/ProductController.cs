573b3618f5a570af7b14213c52f37e8a9381712e
Oleg Tsybulskyi <helg37@gmail.com>
2022-05-18 16:58:58 +0300

    Add ShopOnline.Api.Testing.

---
+++ ShopOnline.Api.Testing/UnitTests/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopOnline.Api.Testing.UnitTests
+{
+    public class ProductController
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal(1, 2);
+        }
+    }
+}
